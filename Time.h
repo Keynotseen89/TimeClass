496e35f9974dfcef206297b397327d02bcd7315f
Keynotseen89 <ksintora@yahoo.com>
2017-08-17 16:55:26 -0700

    Update Time.h
    added comments to these files

--- Time.h
+++ Time.h
@@ -1,19 +1,21 @@
 #pragma once
+//header class for Time
 class Time
 {
 public:
-	Time();
-	Time(int, int);
-	void setHours(int);
-	void setMinutes(int);
-	int getHours();
-	int getMinutes();
-	void print();
-	int minSinceMidnight() const;
-	void advance();
-	~Time();
+	//Prototype
+	Time();								//default function			
+	Time(int, int);						//mutator function
+	void setHours(int);					//setHours function
+	void setMinutes(int);				//setMinutes function
+	int getHours();						//getHours function
+	int getMinutes();					//getMinutes function
+	void print();						//prints function
+	int minSinceMidnight() const;		//function for minutes since midnight
+	void advance();						//advance function 
+	~Time();							//Desturtor
 private:
-	int hour;
-	int minute;
+	int hour;							//integer value of hour
+	int minute;							//integer value of minute
 };
 
